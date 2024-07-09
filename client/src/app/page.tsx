130d87aafd4e100e6da2350021a6f25892183b74
viettrungIT3 <viettrungcntt03@gmail.com>
2024-07-10 00:23:29 +0700

    feat(mode theme): :sparkles: Dark Light Mode with Shadcn UI Next.js

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold">Hello World</h1>
+      <div>
+        Mode theme: <ModeToggle />
+      </div>
     </main>
   );
 }
